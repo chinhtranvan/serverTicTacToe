31a53c002eeed98d47d553550892de47b7ef191d
chinhtranvan <47764275+chinhtranvan@users.noreply.github.com>
2022-04-29 11:35:23 -0400

    variable port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
